b00555ba74403ece9cacc816497cc804cc57aa61
GLOBALAD\nicolas.puccar <nicolas.puccar@globalsis.com.ar>
2025-06-02 11:07:31 -0300

    cambios varios

--- src/GS.Certifications.Web/Areas/Security/Pages/Login.Designer.cs
+++ src/GS.Certifications.Web/Areas/Security/Pages/Login.Designer.cs
@@ -70,6 +70,16 @@ namespace GS.Certifications.Web.Areas.Security.Pages {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Algo no salió bien al enviar la notificación de la recuperación de contraseña. Intentá más tarde o consultá a soporte..
+        /// </summary>
+        public static string Algo_no_salió_bien_al_enviar_la_notificación_de_la_recuperación_de_contraseña__Intentá_más_tarde_o_consultá_a_soporte_ {
+            get {
+                return ResourceManager.GetString("Algo no salió bien al enviar la notificación de la recuperación de contraseña. In" +
+                        "tentá más tarde o consultá a soporte.", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Cancelar.
         /// </summary>
         public static string Cancelar {
@@ -106,11 +116,29 @@ namespace GS.Certifications.Web.Areas.Security.Pages {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Debe ingresar un usuario o email..
+        /// </summary>
+        public static string Debe_ingresar_un_usuario_o_email_ {
+            get {
+                return ResourceManager.GetString("Debe ingresar un usuario o email.", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to el documento actual.
         /// </summary>
         public static string el_documento_actual {
             get {
                 return ResourceManager.GetString("el documento actual", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to El usuario no existe. Complete con un email válido..
+        /// </summary>
+        public static string El_usuario_no_existe__Complete_con_un_email_válido_ {
+            get {
+                return ResourceManager.GetString("El usuario no existe. Complete con un email válido.", resourceCulture);
             }
         }
         
@@ -138,6 +166,15 @@ namespace GS.Certifications.Web.Areas.Security.Pages {
         public static string Grupo {
             get {
                 return ResourceManager.GetString("Grupo", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Ha finalizado la sesión.
+        /// </summary>
+        public static string Ha_finalizado_la_sesión {
+            get {
+                return ResourceManager.GetString("Ha finalizado la sesión", resourceCulture);
             }
         }
         
@@ -241,11 +278,29 @@ namespace GS.Certifications.Web.Areas.Security.Pages {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to por favor revise su casilla de correo..
+        /// </summary>
+        public static string por_favor_revise_su_casilla_de_correo_ {
+            get {
+                return ResourceManager.GetString("por favor revise su casilla de correo.", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Portal de Certificaciones.
         /// </summary>
         public static string Portal_de_Certificaciones {
             get {
                 return ResourceManager.GetString("Portal de Certificaciones", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Se ha enviado un email a.
+        /// </summary>
+        public static string Se_ha_enviado_un_email_a {
+            get {
+                return ResourceManager.GetString("Se ha enviado un email a", resourceCulture);
             }
         }
         
@@ -268,11 +323,11 @@ namespace GS.Certifications.Web.Areas.Security.Pages {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Usuario o Email.
+        ///   Looks up a localized string similar to Usuario o clave incorrecta..
         /// </summary>
-        public static string Usuario_o_Email {
+        public static string Usuario_o_clave_incorrecta_ {
             get {
-                return ResourceManager.GetString("Usuario o Email", resourceCulture);
+                return ResourceManager.GetString("Usuario o clave incorrecta.", resourceCulture);
             }
         }
         
